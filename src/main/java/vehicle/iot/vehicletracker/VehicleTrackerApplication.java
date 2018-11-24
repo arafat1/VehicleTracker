50b94117fd038018f9688dfc15c6a26068de50f2
Mohd Arafat <marafat@bgsu.edu>
2018-11-24 10:57:23 -0500

    add project skeleton

---
+++ src/main/java/vehicle/iot/vehicletracker/VehicleTrackerApplication.java
@@ -0,0 +1,12 @@
+package vehicle.iot.vehicletracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleTrackerApplication.class, args);
+	}
+}
