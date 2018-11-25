ca7f537aab9cf2e006160266e3d44c02829e7a67
invin1 <marafat@bgsu.edu>
2018-11-24 23:28:12 -0500

    add Vehicle Controller

---
+++ src/main/java/vehicle/iot/vehicletracker/controller/VehicleController.java
@@ -0,0 +1,45 @@
+package vehicle.iot.vehicletracker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import vehicle.iot.vehicletracker.entity.Vehicle;
+import vehicle.iot.vehicletracker.service.VehicleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "vehicles")
+public class VehicleController {
+
+    private VehicleService vehicleService;
+
+    @Autowired
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Vehicle> findAll() {
+        return vehicleService.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="{vin}")
+    public Vehicle findOne(@PathVariable String vin) {
+        return vehicleService.findOne(vin);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Vehicle create(@RequestBody Vehicle vehicle) {
+        return vehicleService.create(vehicle);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "{vin}")
+    public Vehicle update(@PathVariable String vin, @RequestBody Vehicle vehicle) {
+        return vehicleService.update(vin, vehicle);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "{vin}")
+    public void delete(@PathVariable String vin) {
+        vehicleService.delete(vin);
+    }
+}
