89bc30e34f74e9548336da47fdf67a0059f6cd69
invin1 <marafat@bgsu.edu>
2018-11-24 23:37:53 -0500

    add respnse status annotation to exceptions

--- src/main/java/vehicle/iot/vehicletracker/exception/BadRequestException.java
+++ src/main/java/vehicle/iot/vehicletracker/exception/BadRequestException.java
@@ -1,5 +1,10 @@
 package vehicle.iot.vehicletracker.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
 
     public BadRequestException(String msg) {
--- src/main/java/vehicle/iot/vehicletracker/exception/ResourceNotFoundException.java
+++ src/main/java/vehicle/iot/vehicletracker/exception/ResourceNotFoundException.java
@@ -1,8 +1,10 @@
 package vehicle.iot.vehicletracker.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseBody()
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends  RuntimeException {
 
     public ResourceNotFoundException(String msg) {
